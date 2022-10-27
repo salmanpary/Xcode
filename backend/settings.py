89060d0d88b2ff512bf40b4556dd27b3ad981b95
bijoyvbabu123 <73835543+bijoyvbabu123@users.noreply.github.com>
2022-10-27 15:03:57 +0530

    created the app

--- backend/settings.py
+++ backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'xcodebackend.apps.XcodebackendConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
