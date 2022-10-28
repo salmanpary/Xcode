fae7683c394ae254302e77ec56b3476c8aaf7ab6
Vishnurr2k01 <vishnurr2k01@gmail.com>
2022-10-28 08:35:46 +0530

    m

--- frontend/pages/api/openai.js
+++ frontend/pages/api/openai.js
@@ -1,17 +1,18 @@
 const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
-    apiKey: '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'  })
+  apiKey: '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'
+})
 
 export default async (req, res) => {
   let prompt = 'node js code for login';
-  console.log(prompt,'prompt')
+  console.log(prompt, 'prompt')
   const openai = new OpenAIApi(configuration);
-const response = await openai.createCompletion({
-  model: "text-davinci-002",
-  prompt: prompt,
-  max_tokens: 100,
-  temperature: 0.9,
-});
+  const response = await openai.createCompletion({
+    model: "text-davinci-002",
+    prompt: prompt,
+    max_tokens: 100,
+    temperature: 0.9,
+  });
 
-  res.status(200).json({text: `${response.data.choices[0].text}`})
+  res.status(200).json({ text: `${response.data.choices[0].text}` })
 }
