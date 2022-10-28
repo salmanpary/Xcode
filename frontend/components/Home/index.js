3388097762b24c85318d818cee7660e5d4961e2a
salmanpary <salmanpary@gmail.com>
2022-10-28 07:37:23 +0530

    dfaf

--- frontend/components/Home/index.js
+++ frontend/components/Home/index.js
@@ -3,6 +3,30 @@ import Image from "next/image";
 import React, { useEffect } from "react";
 import Link from "next/link";
 function HomePage() {
+  const plans = [
+    {
+      plan: "Basic",
+      feature1: "maximum 1 project",
+      feature2: "10000 API calls",
+      feature3: "No code Api builder",
+      rate: "$0",
+    },
+    {
+      plan: "Pro",
+      feature1: "maximum 5 project",
+      feature2: "100000 API calls",
+      feature3: "10gb storage",
+      rate: "$60/month",
+    },
+    {
+      plan: "Scale",
+
+      feature1: "Auto generated code snippets",
+      feature2: "1000000 API calls",
+      feature3: "50gb storage",
+      rate: "$120/month",
+    },
+  ];
   useEffect(() => {
     axios
       .get("https://xcode321.herokuapp.com/getalltable")
@@ -27,7 +51,7 @@ function HomePage() {
           <li className="text-[#4E7AEA] mr-12">Home</li>
           <li className="text-white mr-12">Pricing</li>
           <Link href={"/dashboard"}>
-          <li className="text-white mr-12">Dashboard</li>
+            <li className="text-white mr-12">Dashboard</li>
           </Link>
           <li className="text-text-prm mr-12">Log in/Sign up</li>
         </ul>
@@ -40,15 +64,21 @@ function HomePage() {
       </div>
       <h3 className="text-5xl text-center font-bold text-text-prm">Pricing</h3>
       <div className=" flex pb-12 justify-evenly mt-20">
-        <PriceCard />
-        <PriceCard />
-        <PriceCard />
+        {plans.map((plan) => (
+          <PriceCard
+            plan={plan.plan}
+            feature1={plan.feature1}
+            feature2={plan.feature2}
+            feature3={plan.feature3}
+            rate={plan.rate}
+          />
+        ))}
       </div>
     </div>
   );
 }
 
-const PriceCard = () => {
+const PriceCard = ({ plan, feature1, feature2, feature3, rate }) => {
   return (
     <div
       style={{
@@ -60,14 +90,14 @@ const PriceCard = () => {
       className="text-white p-12 flex flex-col items-center "
     >
       <h3 className="text-5xl font-bold text-white min-w-[10rem] rounded-lg">
-        Plan 1
+        {plan}
       </h3>
       <ul className="text-3xl mt-8">
-        <li className="mt-4">Feature</li>
-        <li className="mt-4">Feature</li>
-        <li className="mt-4">Feature</li>
+        <li className="mt-4">{feature1}</li>
+        <li className="mt-4">{feature2}</li>
+        <li className="mt-4">{feature3}</li>
       </ul>
-      <h3 className="text-5xl font-bold mt-6">Rate</h3>
+      <h3 className="text-5xl font-bold mt-6">{rate}</h3>
     </div>
   );
 };
